037871097a15d0e47ab39d338418d61cfeba171b
devinsimmons <devinsimmons71@gmail.com>
2020-08-19 22:01:06 -0400

    first commit. I have a map that displays bus positions that are refreshed every 5 seconds. basic info is availabale on click

--- src/App.js
+++ src/App.js
@@ -1,26 +1,144 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+//
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import mapboxgl from 'mapbox-gl';
+import './mapbox-gl.css';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+
+const API_KEY = '6351dad7ec9d4c4f9f03bab9b5180c38';
+const BUS_POSITIONS = 'https://api.wmata.com/Bus.svc/json/jBusPositions';
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2ltbW9uc2QiLCJhIjoiY2poeXk3YzlpMHJsbTNwcnYyNW1zeG9vMCJ9.sRhhJsrU0qUGbM7LiSrW_Q';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.getBusPositions = this.getBusPositions.bind(this);
+  }
+
+  state = {
+    lat: 38.9,
+    lng: -77.1,
+    zoom: 11,
+    bus_json: {
+      'type': 'FeatureCollection',
+      'features': []
+    }
+  }
+
+  componentDidMount() {
+    const {bus_json} = this.state;
+
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+
+    //creating a new variable that points to the outer this,
+    //ie the this that is associated with the app component
+    const self = this;
+    //the scope of this changes inside of the below function
+    this.map.on('load', function () {
+      //add the points to the map
+      this.addSource('buses', {
+        'type': 'geojson',
+        'data': {...self.state.bus_json}
+      });
+      this.addLayer({
+        id: 'buses',
+        type: 'circle',
+        source: 'buses',
+        paint: {
+          'circle-color': '#546A7B'
+        }
+      });
+    });
+
+    this.map.on('click', 'buses', function(e) {
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const route_id = e.features[0].properties.RouteID;
+      const headsign = e.features[0].properties.TripHeadsign;
+       
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+       
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(route_id + ' to ' + headsign)
+        .addTo(this)
+    });
+
+    this.getBusPositions();
+    //this runs the bus position function every 5 seconds
+    this.timer_id = setInterval(() => this.getBusPositions(), 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer_id);
+  }
+
+  componentDidUpdate() {
+    //prevents an error from being thrown. when the page loads, this function is called
+    //before the map loads
+    if(typeof(this.map.getSource('buses')) !== 'undefined') {
+      this.map.getSource('buses').setData(this.state.bus_json);
+    }
+  }
+
+  //this function is called when the page loads and every 10 seconds after that
+  getBusPositions() {
+    axios
+      .get(BUS_POSITIONS, {
+        headers: {
+          'api_key': API_KEY
+        }
+      })
+      .then( res => {
+        const new_bus_json = {
+          'type': 'FeatureCollection',
+          'features': []
+        };
+        const updated_buses = [];
+    
+        //looping over each feature from the request
+        for (var feature of res.data.BusPositions) {
+          const feat_obj = {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [
+                feature.Lon,
+                feature.Lat
+              ]
+            },
+            'properties': {...feature}
+          }
+          updated_buses.push(feat_obj);
+        }
+        new_bus_json['features'] = updated_buses;
+        this.setState({bus_json: new_bus_json});
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render () {
+    return(
+      <div>
+        <div ref={el => this.mapContainer = el} className="mapContainer" />
+      </div>
+    );
+  }
 }
 
 export default App;
